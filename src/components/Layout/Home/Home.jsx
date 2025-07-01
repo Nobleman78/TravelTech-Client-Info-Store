ffaf68039b528fcfaa78df355e8331ef6281b32c
Jesmin Chakma <jesminchakma39@gmail.com>
2025-07-01 18:30:26 +0600

    Updated

--- src/components/Layout/Home/Home.jsx
+++ src/components/Layout/Home/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
         const clientInfo = { name, phoneNumber, purpose };
 
         try {
-            const response = await fetch('https://client-information-server.vercel.app/client-information', {
+            const response = await fetch('https://client-server-taupe.vercel.app/client-information', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
