14a5c6bf81e37a7b682ec50c37fccb9e4498ef12
Jesmin Chakma <jesminchakma39@gmail.com>
2025-07-05 14:59:06 +0600

    Updated

--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import React, { useContext} from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import AuthContext from '../components/Context/Authcontext';
 
@@ -6,15 +6,17 @@ const ProtectedRoute = () => {
     const { user, loading } = useContext(AuthContext);
     const location = useLocation()
     const navigate = useNavigate()
- 
+    useEffect(() => {
+        if (!loading && !user) {
+            return navigate('/login', { replace: true, state: { from: location } });
+        }
+    }, [location, user, navigate, loading])
     if (loading) {
         return <div className="text-center py-10">
             <span className="text-2xl font-medium text-green-600 animate-pulse">Loading ...</span>
         </div>
     }
-    if (!user) {
-        return navigate('/login', { replace: true, state: { from: location } });
-    }
+
 
     return <Outlet />;
 };
